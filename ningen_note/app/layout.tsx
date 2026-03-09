6f27ec159d0898eaf85c4db88e187b11d3096cda
ningengakushu-dot <ningengakushu@gmail.com>
2026-03-10 02:21:27 +0900

    Supabaseクライアント(client.ts/server.ts)、Prismaクライアント(server.ts) OK

--- ningen_note/app/layout.tsx
+++ ningen_note/app/layout.tsx
@@ -3,8 +3,6 @@ import "./globals.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
-
-
 export const metadata: Metadata = {
   title: "NINGEN GAKUSHU NOTE",
   description: "個人用ブログ兼CMS",
--- ningen_note/components/Header.tsx
+++ ningen_note/components/Header.tsx
@@ -2,7 +2,6 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-
         <header>
             <div>
                 <Link href="/">NINGEN GAKUSHU NOTE</Link>
