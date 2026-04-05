886e644ecd83db4192497caee82712343debdb76
ningengakushu-dot <ningengakushu@gmail.com>
2026-04-05 21:55:16 +0900

    feat: Google Analytics 4 のトラッキングコードを追加

--- ningen_note/app/layout.tsx
+++ ningen_note/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Inter, Noto_Sans_JP, M_PLUS_Rounded_1c } from "next/font/google"
 import "./globals.css"
 import Footer from "@/components/Footer"
-
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"], variable: "--font-noto-sans-jp" })
@@ -21,6 +21,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      {/* GAトラッキングコード */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-0PNW8YF93Y" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || []
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date())
+        gtag('config', 'G-0PNW8YF93Y')
+        `}
+      </Script>
+
       <body className={`${inter.variable} ${notoSansJP.variable} ${mPlusRounded.variable}`}>
         {children}
         <Footer />
